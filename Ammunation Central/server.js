2ef24880625e8b177e18c60f679b3e2a3e7c9784
Daniel Ruiz <srdani.contact@gmail.com>
2026-09-25 12:46:38 +0200

    Web Ammu 1

--- Ammunation Central/server.js
+++ Ammunation Central/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const session = require("express-session");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const path = require("path");
 
 const db = require("./database");
